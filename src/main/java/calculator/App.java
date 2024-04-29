bd8f34c528f2f8b498161c3ffafdd5acedddd8b0
Robbie <robbie331@>
2024-04-29 11:41:25 +0900

    ãLv1 요ã구구사항 2 완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -14,6 +14,10 @@ public class App {
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num2 = sc.nextInt();
 
-        System.out.println("nun1, num2 = " + num1 + ", " + num2);
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        // next()로 String으로 받고, charAt(0)으로 문자열->문자 변환 (한글자니 인덱스 0 고정)
+        char operator = sc.next().charAt(0);
+
+        System.out.println("operator = " + operator);
     }
 }
