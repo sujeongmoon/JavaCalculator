f26c3ec5af3768ba219d5f3ca18c8bf6e7726f4b
Robbie <robbie331@>
2024-04-29 20:22:35 +0900

    Lv 2-7) 원의 넓이 계산해서 반환해주는 메서드 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,15 +1,23 @@
 package calculator;
-
 import java.util.ArrayList;
 
 public class Calculator {
+    //원의 넓이 구하기
+    private int r; //반지름
+    private static final double PI = 3.14; //원주율 -변하지않는고정된 상수
     // 연산 결과를 저장하는 컬렉션 타입 필드
     private ArrayList<Double> resultArrayList;
+    // 원의 넓이 결과를 저장하는 컬렉션 타입 필드
+    private ArrayList<Double> resultCircleArea;
+
 
     //생성자
     Calculator(){
-        this.resultArrayList = new ArrayList<Double>(); //초기화
+        //초기화
+        this.resultArrayList = new ArrayList<Double>();
+        this.resultCircleArea = new ArrayList<Double>();
     }
+
 
 
     // 연산기능을 수행한 후 결과값을 반환하는 메서드, 예외처리
@@ -55,6 +63,31 @@ public class Calculator {
     public void inquiryResultArrayList(){
         for (double rsArrList : this.resultArrayList) {
             System.out.print(rsArrList + " ");
+        }
+        System.out.println();
+    }
+
+    //원의 넓이를 구하는 메서드
+    public double calculateCircleArea(int r) {
+        double result = r*r*PI;
+        this.resultCircleArea.add(result); //원의 넓이 저장
+        return result;
+    }
+
+    // 원의 넓이 결과값 배열리스트 가져오기
+    public ArrayList<Double> getResultCircleArea(){
+        return this.resultCircleArea;
+    }
+
+    //원의 넓이 결과값 배열리스트 수정하기
+    public void setResultCircleArea(ArrayList<Double> resultCircleArea){
+        this.resultCircleArea = resultCircleArea;
+    }
+
+    // 원의 넓이 결과 조회
+    public void inquiryCircleArea(){
+        for (double rr : this.resultCircleArea) {
+            System.out.print(rr + " ");
         }
         System.out.println();
     }
