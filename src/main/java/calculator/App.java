4c52fa95136353fab5ed60b089679a5481da2f3c
Robbie <robbie331@>
2024-04-29 12:05:08 +0900

    Lv 1-3) 제어문 사용

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -18,6 +18,27 @@ public class App {
         // next()로 String으로 받고, charAt(0)으로 문자열->문자 변환 (한글자니 인덱스 0 고정)
         char operator = sc.next().charAt(0);
 
-        System.out.println("operator = " + operator);
+        /*입력받은 값으로 계산하기*/
+        int result = 0;
+        if (operator == '+'){
+            result = num1 + num2;
+        } else if (operator == '-'){
+            result = num1 - num2;
+        } else if (operator == '*'){
+            result = num1 * num2;
+        } else if (operator == '/'){
+            // 분모가 0인 경우 return
+            if (num2 == 0){
+                System.out.println("나눗셈에서 분모는 0일 수 없습니다.");
+                return;
+            }
+            result = num1 / num2;
+        }
+        //연산자 잘못 입력한 경우 return
+        else {
+            System.out.println("연산자를 잘못 입력하였습니다.");
+            return;
+        }
+        System.out.println("result = " + result);
     }
 }
