471570936006b1eba691d494f9fcffe0b47cce27
Robbie <robbie331@>
2024-04-29 16:48:38 +0900

    Lv 2-1) Calculator 클래스, 예외처리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,34 @@
 package calculator;
 
-public class Calculator {
+import java.util.ArrayList;
 
+public class Calculator {
+    // 연산 결과를 저장하는 컬렉션 타입 필드
+    ArrayList<Integer> resultArrayList = new ArrayList<Integer>();
+    int num1;
+    int num2;
+    char operator;
+
+    // 연산기능을 수행한 후 결과값을 반환하는 메서드, 예외처리
+    public double calculate(int num1, int num2, char operator) throws BadInputException{
+        double result = 0;
+
+        if (operator == '+') {
+            result = num1 + num2;
+        } else if (operator == '-') {
+            result = num1 - num2;
+        } else if (operator == '*') {
+            result = num1 * num2;
+        } else if (operator == '/') {
+            if (num2 == 0){
+                throw new BadInputException("분모 0");
+            }
+            result = (double)num1 / num2;
+        } else{
+            throw new BadInputException("연산자 입력오류"); // 연산자가 안 들어온 경우 예외처리
+        }
+
+        /* return 연산 결과 */
+        return result;
+    }
 }
